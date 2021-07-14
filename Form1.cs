2ed09d59fd141c6bd234bc13c039de4e8da0b960
johnthe4 <67112212+johnthe4@users.noreply.github.com>
2021-07-13 21:42:55 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //close the application
+            this.Close();
+
+        }
+
+
+        private void txtUsername_MouseClick(object sender, MouseEventArgs e)
+        {
+            //clear textbox upon user click
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Clear();
+            }
+        }
+
+        private void txtPassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            //clear textbox upon user click
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Clear();
+                txtPassword.PasswordChar = '*';     //hide password that is being typed by user
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
